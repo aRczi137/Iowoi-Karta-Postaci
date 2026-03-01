92429d970879147d5d8864dad5305bf3796e72c8
aRczi <.>
2026-03-01 21:11:08 +0100

    update gemini.ts

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -3,7 +3,7 @@ import { Character } from '../types';
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY || "" });
 
-export const geminiModel = "gemini-3-flash-preview";
+export const geminiModel = "gemini-flash-latest";
 export const imageModel = "gemini-3.1-flash-image-preview";
 
 export async function generateCharacterAvatar(
